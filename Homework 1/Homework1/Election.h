a04a4662a6c294a409aca68ae6a16135c313de57
Dorsg <Dorsg@users.noreply.github.com>
2020-12-24 20:49:00 +0200

    added funcion displayResults

--- Homework 1/Homework1/Election.h
+++ Homework 1/Homework1/Election.h
@@ -17,34 +17,41 @@ namespace elections {
 		PartiesArr partiesArr;
 		Date date;
 	public:
-		Election() {	}
+		Election() {}
 		Election(Date& _date, DistrictsArr& _districtsArr, PartiesArr& _partiesArr, CitizensDB& _citizensDB) :
 			date(_date), districtsArr(_districtsArr), partiesArr(_partiesArr), citizensDB(_citizensDB) {}
 
 		virtual void displayResults() = 0;
+		~Election() {}
 	};
 
 	class RegularElection : public Election {
+	private:
+		CitizensDB electorsByDistrict;
+
 	public:
 		RegularElection(Date& _date, DistrictsArr& _districtsArr, PartiesArr& _partiesArr, CitizensDB& _citizensDB)
-			: Election(_date, _districtsArr, _partiesArr, _citizensDB)	{}
+			: Election(_date, _districtsArr, _partiesArr, _citizensDB), electorsByDistrict(_districtsArr.getLogSize())	{}
 
-		virtual void displayResults() {};
+		virtual void displayResults();
+		void getElectedRepsFromAllDistricts();
+		~RegularElection();
 
 	};
 
 
 	class SimpleElection : public Election {
+	private:
+		CitizensArr electors;
 	public:
 		SimpleElection(Date& _date, DistrictsArr& _districtsArr, PartiesArr& _partiesArr, CitizensDB& _citizensDB)
 			: Election(_date, _districtsArr, _partiesArr, _citizensDB) {}
 
 		virtual void displayResults() {};
+		~SimpleElection();
 
 	};
 
-
-	
 
 
 
