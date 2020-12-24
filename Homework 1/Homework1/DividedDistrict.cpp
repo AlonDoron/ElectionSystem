02735238a2d55019dbd0394255d28e40734ca522
Dorsg <Dorsg@users.noreply.github.com>
2020-12-24 20:47:57 +0200

    added getElctedReps

--- Homework 1/Homework1/DividedDistrict.cpp
+++ Homework 1/Homework1/DividedDistrict.cpp
@@ -7,7 +7,32 @@ namespace elections {
 	DividedDistrict::DividedDistrict() : District() {};
 
 	DividedDistrict::DividedDistrict(char* _name, int _nameLen, int _numOfRep, int _districtNum)
-		: District(_name, _nameLen, _numOfRep, _districtNum) {};
+		: District(_name, _nameLen, _numOfRep, _districtNum) {}
+	CitizensArr DividedDistrict::getElctedReps(PartiesArr* partiesAr)
+	{
+		//int currRepsNum;
+		//int partyNum = _partiesArr->getLogSize();
+		CitizensArr res, curr, RepsInPartyi;
+		//int* votesPerc = votesCounter.getPercentageVotes();
+		//PartiesArr partiesArr = *_partiesArr;
+		//Party currParty;
+		//CitizensDB currDB;
+
+		//int winningParty = votesCounter.getWinningPartID();
+
+		//for (int i = 0; i < partyNum; i++)
+		//{	// calculate how many reps from each party
+		//	currParty = partiesArr[i];
+		//	currDB = currParty.getRepresentatives();
+		//	RepsInPartyi = currDB[districtNum];
+		//	currRepsNum = (votesPerc[i] * numOfRep) / 100;
+		//	curr = RepsInPartyi.getCitizensUntillIndex(currRepsNum);
+		//	res.appendCitizensArr(curr);
+		//}
+
+		return res;
+	}
+	;
 
 
 }
--- Homework 1/Homework1/DividedDistrict.h
+++ Homework 1/Homework1/DividedDistrict.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "District.h"
+#include "CitizensArr.h"
+
 
 namespace elections {
-
+	class CitizensArr;
 class DividedDistrict : public District {
 
 public:
 	DividedDistrict();
 	DividedDistrict(char* _name, int _nameLen, int _numOfRep, int _districtNum);
 
+
+	virtual CitizensArr getElctedReps(PartiesArr* partiesArr);
 };
 
 }
